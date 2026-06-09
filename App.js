d77d2cde704cf9557a3121d32b6ab016705cd904
Rafael Mendes Neves <rafael3a2e4@gmail.com>
2026-06-09 15:51:26 -0300

    feat: configuração de navegação com Stack Navigator

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./src/screens/LoginScreen";
+import CadastroScreen from "./src/screens/CadastroScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import TarefasDetalheScreen from "./src/screens/TarefasDetalheScreen";
+import TarefaScreen from "./src/screens/TarefaScreen";
+import PostScreen from "./src/screens/PostScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ title: "login" }} />
+        <Stack.Screen name="Cadastro" component={CadastroScreen} options={{ title: "cadastro" }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: "home" }} />
+        <Stack.Screen name="TarefasDetalhe" component={TarefasDetalheScreen} options={{ title: "detalhes da tarefa" }} />
+        <Stack.Screen name="Tarefa" component={TarefaScreen} options={{ title: "tarefa" }} />
+        <Stack.Screen name="Post" component={PostScreen} options={{ title: "post" }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+}   
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
